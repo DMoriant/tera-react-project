cfb1ef33b5376b974521b32e9d1b6e84584d11fc
DMoriant <120055089+DMoriant@users.noreply.github.com>
2023-06-05 22:05:32 -0300

    html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Home from "./components/pages/Home";
+import UserPostForm from "./components/pages/UserPostForm";
 
 import "./styles/normalize.css";
 import "./styles/fontawesome.min.css";
 import "./styles/main.css";
 
 function App() {
-  return <Home />;
+  return <UserPostForm />;
 }
 
 export default App;
